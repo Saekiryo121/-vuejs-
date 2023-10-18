689caa7c340c18e7bc289bdc6f100fb84089f6e8
saeki <0ec6ky47385587v@gmail.com>
2023-10-18 17:04:57 +0900

    ソート機能降順・昇順

--- vue.js
+++ vue.js
@@ -8,12 +8,16 @@ Vue.createApp({
   methods: {
     sortTable: function () {
       if (this.selectedSort === 'up') {
-        this.products.sort((a, b) => a.id - b.id);
+        this.products.sort(function (a, b) {
+          return a.id - b.id;
+        });
       } else if (this.selectedSort === 'down') {
-        this.products.sort((a, b) => b.id - a.id);
+        this.products.sort(function (a, b) {
+          return b.id - a.id;
+        });
       }
     }
-  },
+  },  
   created: async function () {
       let res = await fetch('./package.json');
       let items = await res.json();
