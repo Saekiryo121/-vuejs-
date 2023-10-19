2fd366c491d33b4380accf4dcded09a6dc223240
saeki <0ec6ky47385587v@gmail.com>
2023-10-19 18:07:30 +0900

    idを数値型に変換処理（失敗）

--- vue.js
+++ vue.js
@@ -6,31 +6,37 @@ Vue.createApp({
       searchKeyword: "",
       products: [],
       allProducts: [],
-      selectedSortKey: "id",
+      selectedSortKey: "name",
       sortDirection: 1,
     };
   },
   methods: {
-   
-    customSortFunction: function (key) {
-      if (this.selectedSortKey === key) {
-        this.sortDirection *= -1;
-      } else {
-        this.selectedSortKey = key;
-        this.sortDirection = 1;
+    sortTable: function () {
+      if (this.selectedSort === "up") {
+        this.products.sort(function (a, b) {
+          return a.id - b.id;
+        });
+      } else if (this.selectedSort === "down") {
+        this.products.sort(function (a, b) {
+          return b.id - a.id;
+        });
       }
-
-      console.log("Selected Sort Key:", key);
-      console.log("Sort Direction:", this.sortDirection);
-
-      this.products.sort((a, b) => {
-        let aValue = a[this.selectedSortKey];
-        let bValue = b[this.selectedSortKey];
-        let sortFactor = this.sortDirection;
-
-        return sortFactor * aValue.localeCompare(bValue, "ja");
-      });
     },
+    customSortFunction(key) {
+    if (this.selectedSortKey === key) {
+      this.sortDirection *= -1;
+    } else {
+      this.selectedSortKey = key;
+      this.sortDirection = 1;
+    }
+    this.products.sort((a, b) => {
+      let aValue = key === 'id' ? Number(a[key]) : a[key].toLowerCase();
+      let bValue = key === 'id' ? Number(b[key]) : b[key].toLowerCase();
+      let sortFactor = this.sortDirection;
+
+      return sortFactor * aValue.localeCompare(bValue, 'ja');
+    })
+  },
     sortTables: function () {
       if (this.selectedSort === "id") {
         this.products.sort(function (a, b) {
