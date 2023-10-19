0b6b4da3eebe2b358dea19904dda01fd1a8ab37c
saeki <0ec6ky47385587v@gmail.com>
2023-10-19 12:57:35 +0900

    キーワド検索機能

--- vue.js
+++ vue.js
@@ -1,28 +1,66 @@
 Vue.createApp({
-  data: function() {
+  data: function () {
     return {
-      selectedSort: 'up',
-      selectSort: 'id',
-      searchKeyword: '',
-      products: []
+      selectedSort: "up",
+      selectSort: "id",
+      searchKeyword: "",
+      products: [],
     };
   },
   methods: {
     sortTable: function () {
-      if (this.selectedSort === 'up') {
+      if (this.selectedSort === "up") {
         this.products.sort(function (a, b) {
           return a.id - b.id;
         });
-      } else if (this.selectedSort === 'down') {
+      } else if (this.selectedSort === "down") {
         this.products.sort(function (a, b) {
           return b.id - a.id;
         });
       }
-    }
+    },
+    sortTables: function () {
+      if (this.selectedSort === "id") {
+        this.products.sort(function (a, b) {
+          return a.id - b.id;
+        });
+      } else if (this.selectedSort === "name") {
+        this.products.sort(function (a, b) {
+          return a.name.localeCompare(b.name);
+        });
+      } else if (this.selectedSort === "company") {
+        this.products.sort(function (a, b) {
+          return a.company.localeCompare(b.company);
+        });
+      } else if (this.selectedSort === "division") {
+        this.products.sort(function (a, b) {
+          return a.division.localeCompare(b.division);
+        });
+      } else if (this.selectedSort === "title") {
+        this.products.sort(function (a, b) {
+          return a.title.localeCompare(b.title);
+        });
+      }
+    },
+    search: function () {
+      let keyword = this.searchKeyword.toLowerCase();
+      if (keyword === "") {
+        this.products = this.products;
+      } else {
+        this.products = this.products.filter(function (product) {
+          return (
+            product.name.toLowerCase().includes(keyword) ||
+            product.company.toLowerCase().includes(keyword) ||
+            product.division.toLowerCase().includes(keyword) ||
+            product.title.toLowerCase().includes(keyword)
+          );
+        });
+      }
+    },
   },
   created: async function () {
-      let res = await fetch('./package.json');
-      let items = await res.json();
-      this.products = items;
-    }
+    let res = await fetch("./package.json");
+    let items = await res.json();
+    this.products = items;
+  },
 }).mount("#app");
