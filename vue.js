93e2759512cc2e7cbed9e0b25e18ea18c9c59b55
saeki <0ec6ky47385587v@gmail.com>
2023-10-20 12:57:40 +0900

    新規情報作成（試作品）

--- vue.js
+++ vue.js
@@ -8,7 +8,14 @@ Vue.createApp({
       allProducts: [],
       selectedSortKey: "name",
       sortDirection: 1,
-    }
+      newUserInfo: {
+        id: null,
+        name: "",
+        company: "",
+        division: "",
+        title: ""
+      }
+    };
   },
   methods: {
     sortTable: function () {
@@ -23,57 +30,80 @@ Vue.createApp({
       }
     },
     customSortFunction(key) {
-    if (this.selectedSortKey === key) {
-      this.sortDirection *= -1;
-    } else {
-      this.selectedSortKey = key;
-      this.sortDirection = 1;
-    }
-    this.products.sort((a, b) => {
-      let aValue = key === 'id' ? Number(a[key]) : a[key].toLowerCase();
-      let bValue = key === 'id' ? Number(b[key]) : b[key].toLowerCase();
-      console.log(aValue)
-      let sortFactor = this.sortDirection;
-      if (key === 'id') {
-        return sortFactor * (aValue - bValue);
+      if (this.selectedSortKey === key) {
+        this.sortDirection *= -1;
+      } else {
+        this.selectedSortKey = key;
+        this.sortDirection = 1;
       }
+      this.products.sort((a, b) => {
+        let aValue = key === "id" ? Number(a[key]) : a[key].toLowerCase();
+        let bValue = key === "id" ? Number(b[key]) : b[key].toLowerCase();
+        console.log(aValue);
+        let sortFactor = this.sortDirection;
+        if (key === "id") {
+          return sortFactor * (aValue - bValue);
+        }
 
-      return sortFactor * aValue.localeCompare(bValue, 'ja');
-    })
-  },
+        return sortFactor * aValue.localeCompare(bValue, "ja");
+      });
+    },
     search: function () {
       let keyword = this.searchKeyword.toLowerCase();
       if (keyword === "") {
         this.products = this.allProducts;
       } else {
         this.products = this.allProducts.filter((product) => {
-          if (this.selectSort === 'id' && product.id.toString().toLowerCase().includes(keyword)) {
+          if (
+            this.selectSort === "id" &&
+            product.id.toString().toLowerCase().includes(keyword)
+          ) {
             return true;
-          } else if (this.selectSort === 'name' && product.name.toLowerCase().includes(keyword)) {
+          } else if (
+            this.selectSort === "name" &&
+            product.name.toLowerCase().includes(keyword)
+          ) {
             return true;
-          } else if (this.selectSort === 'company' && product.company.toLowerCase().includes(keyword)) {
+          } else if (
+            this.selectSort === "company" &&
+            product.company.toLowerCase().includes(keyword)
+          ) {
             return true;
-          } else if (this.selectSort === 'division' && product.division.toLowerCase().includes(keyword)) {
+          } else if (
+            this.selectSort === "division" &&
+            product.division.toLowerCase().includes(keyword)
+          ) {
             return true;
-          } else if (this.selectSort === 'title' && product.title.toLowerCase().includes(keyword)) {
+          } else if (
+            this.selectSort === "title" &&
+            product.title.toLowerCase().includes(keyword)
+          ) {
             return true;
           } else {
             return false;
           }
         });
       }
-  },  
-  
-  created: async function () {
-      let res = await fetch('./package.json');
-      let items = await res.json();
-      this.products = items;
+    },
+    addNewUser() {
+      if (this.newUserInfo.name) {
+        let maxId = Math.max(...this.products.map(user => user.id));
+        this.newUserInfo.id = maxId + 1;
+        this.products.push({ ...this.newUserInfo });
+        this.clearForm();
+      }
+    },
+    clearForm() {
+      this.newUserInfo.name = "";
+      this.newUserInfo.company = "";
+      this.newUserInfo.division = "";
+      this.newUserInfo.title = "";
     }
-},
+  },
   created: async function () {
     let res = await fetch("./package.json");
     let items = await res.json();
     this.products = items;
     this.allProducts = items;
-  }
+  },
 }).mount("#app");
